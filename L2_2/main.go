9b29fe78ced595f98f39410f4a536e11dcd9a13e
daniel <frederigo234@gmail.com>
2025-08-23 23:27:58 +0300

    Task L1.2

---
+++ L2_2/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+
+	var Numbers []int
+	for {
+		if !sc.Scan() {
+			break
+		}
+		line := strings.TrimSpace(sc.Text())
+		if line == "" {
+			break
+		}
+		for _, f := range strings.Fields(line) {
+			if n, err := strconv.Atoi(f); err == nil {
+				Numbers = append(Numbers, n)
+			} else {
+				continue
+			}
+		}
+	}
+	var wg sync.WaitGroup
+	wg.Add(len(Numbers))
+	for i := range Numbers {
+		i := i
+		go func() {
+			defer wg.Done()
+			Numbers[i] = Numbers[i] * Numbers[i]
+		}()
+	}
+	wg.Wait()
+
+	fmt.Println(Numbers)
+}
