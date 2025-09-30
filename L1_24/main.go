5f999f08daac9be5519763b557be248da1c97a99
daniel <frederigo234@gmail.com>
2025-09-30 22:53:57 +0300

    L1_24

---
+++ L1_24/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) *Point {
+	return &Point{x: x, y: y}
+}
+
+func (p *Point) DistanceTo(other *Point) float64 {
+	return math.Sqrt(math.Pow(other.x-p.x, 2) + math.Pow(other.y-p.y, 2))
+}
+
+func main() {
+	a := NewPoint(3, 4)
+	b := NewPoint(5, 6)
+	fmt.Println(a.DistanceTo(b))
+}
